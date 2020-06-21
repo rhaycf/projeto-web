a9a5e5b28e5fad95baf30c0218caa855883ae554
Rhayane Fabres <65169776+rhaycf@users.noreply.github.com>
2020-06-21 16:58:10 -0300

    Add files via upload

---
+++ scripts/adotar.js
@@ -0,0 +1,23 @@
+function adotAb() {
+    alert('Olá, Estamos quase lá. Envie um e-mail para abiguail@mail.com com suas informações (telefone, celular...) que entraremos em contato para combinar tudo certinho!!');
+}
+
+function adotAm() {
+    alert('Olá, Estamos quase lá. Envie um e-mail para amelia@mail.com com suas informações (telefone, celular...) que entraremos em contato para combinar tudo certinho!!');
+}
+
+function adotSc() {
+    alert('Olá, Estamos quase lá. Envie um e-mail para scott@mail.com com suas informações (telefone, celular...) que entraremos em contato para combinar tudo certinho!!');
+}
+
+function adotCarm() {
+    alert('Olá, Estamos quase lá. Envie um e-mail para caramelo@mail.com com suas informações (telefone, celular...) que entraremos em contato para combinar tudo certinho!!');
+}
+
+function adotUni() {
+    alert('Olá, Estamos quase lá. Envie um e-mail para uni@mail.com com suas informações (telefone, celular...) que entraremos em contato para combinar tudo certinho!!');
+}
+
+function adotado() {
+    alert('Ops! Nosso amiguinho já foi adotado. Que tal dar uma olhada em outros amiguinhos? (;')
+}
